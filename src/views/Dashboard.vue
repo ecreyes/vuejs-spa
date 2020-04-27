c09b91d992fcda26da7a7216c39bd37b73289da7
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2020-04-27 16:51:14 -0400

    example chart

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,13 +3,17 @@
     <h1 class="headline">Mi Aula</h1>
     <v-row>
       <v-col cols="12" xs="12" md="3">
-        <Aside/>
+        <Aside />
       </v-col>
       <router-view></router-view>
       <v-col cols="12" xs="12" md="3">
         <v-card class="mx-auto">
           <v-card-text>
             <div class="overline mb-4">Anuncios</div>
+            <div class="small">
+              <line-chart :chart-data="datacollection"></line-chart>
+              <button @click="fillData()">Randomize</button>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
@@ -19,11 +23,21 @@
 
 <script>
 import Aside from "@/components/Aside";
+import LineChart from "../components/Linechart";
 import { mapState } from "vuex";
 export default {
   name: "Dashboard",
   components: {
-    Aside
+    Aside,
+    LineChart
+  },
+  data() {
+    return {
+      datacollection: null
+    };
+  },
+  mounted() {
+    this.fillData();
   },
   computed: {
     ...mapState("auth", ["isLogged"])
@@ -31,6 +45,28 @@ export default {
   watch: {
     isLogged() {
       this.$router.replace("/");
+    }
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()]
+          },
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
     }
   }
 };
